d7dcaf8ae96eee23dfb21c741dbbe66f21b67314
Petro Niemkov <pniemkov@gmail.com>
2022-03-21 15:39:03 -0400

    CSS fixed for MUI

---
+++ public/build/precache-manifest.0a2d0ee8fe0c20f8852c7493734f7e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51407832b21ae10cdd5b3f36de6fa68",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "27fa30ad800c53f138e1",
+    "url": "/app/static/css/2.7693b373.chunk.css"
+  },
+  {
+    "revision": "aef4f4d4f08b98a80f45",
+    "url": "/app/static/css/main.194b6798.chunk.css"
+  },
+  {
+    "revision": "27fa30ad800c53f138e1",
+    "url": "/app/static/js/2.4b775888.chunk.js"
+  },
+  {
+    "revision": "65ca118bb256954e5cb23fe094bca979",
+    "url": "/app/static/js/2.4b775888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef4f4d4f08b98a80f45",
+    "url": "/app/static/js/main.604b6971.chunk.js"
+  },
+  {
+    "revision": "8200f7f293e2bc67fe1f",
+    "url": "/app/static/js/runtime-main.27fa4a51.js"
+  }
+]);
